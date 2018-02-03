f98ae715e1fb117127173498582dbca93a0ed9f7
takasaki91 <y.baton@gmail.com>
2018-02-04 05:10:02 +0900

    トップバーの上部にあるボタンをクリックするとツイートしてくれるようにした。

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function(){
+    var sendtweet = function(){
+        $("#Tweetstorm-tweet-box-0").find(".SendTweetsButton").click();    
+    }
+    $(".nav.right-actions").append('<li role="complementary" aria-labelledby="global-new-kusotweet-button" class="topbar-tweet-btn"><button id="global-new-kusotweet-button" class="js-tooltip js-dynamic-tooltip EdgeButton EdgeButton--primary"><span class="text">んほぉぉ！</span></button></li>');
+    $("#global-new-kusotweet-button").on('click',function(){ 
+        $("#global-new-tweet-button").click();
+        $(".tweet-box.rich-editor.is-showPlaceholder").append('<div>んぉぉ！イッぐぅぅ！！</div>');
+        setTimeout(sendtweet,100);
+    });
+});
