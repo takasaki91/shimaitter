3cd882d71bdc945ae84280701a25376428023881
takasaki91 <y.baton@gmail.com>
2018-02-05 21:22:02 +0900

    typoを修正、jquery.jsを追加

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(function(){
     $(".nav.right-actions").append('<li role="complementary" aria-labelledby="global-new-kusotweet-button" class="topbar-tweet-btn"><button id="global-new-kusotweet-button" class="js-tooltip js-dynamic-tooltip EdgeButton EdgeButton--primary"><span class="text">んほぉぉ！</span></button></li>');
     $("#global-new-kusotweet-button").on('click',function(){ 
         $("#global-new-tweet-button").click();
-        $(".tweet-box.rich-editor.is-showPlaceholder").append('<div>んぉぉ！イッぐぅぅ！！</div>');
+        $(".tweet-box.rich-editor.is-showPlaceholder").append('<div>んほぉぉ！イッぐぅぅ！！</div>');
         setTimeout(sendtweet,100);
     });
 });
